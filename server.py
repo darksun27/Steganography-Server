d0ebb6809f8fd789f650b1884db804047a7a32ea
Siddharth Batra <sbsiddharth@gmail.com>
2019-12-10 18:47:08 +0530

    Initial Update

--- server.py
+++ server.py
@@ -1,14 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 import base64
 import ImageSteganography as stego
 import cipher_key as text_cipher
 import PIL
-from flask_ngrok import run_with_ngrok
 
 
 app = Flask(__name__)
-run_with_ngrok(app)
 
 img = None
 
@@ -22,7 +20,7 @@ def encode():
             imageStr = stego.encode_image(text_to_encode).decode('utf-8')
             with open("file.txt","w") as t:
                 t.write(imageStr)
-            return {"key" : key, "image":imageStr}
+            return jsonify({"key" : key, "image":imageStr})
     except:
         return "Error",500
 
@@ -36,11 +34,11 @@ def decode():
             try:
                 print(text)
                 plain_text = text_cipher.originalText(text, request.form.get('key'))
-                return {"text":plain_text}
+                return jsonify({"text":plain_text})
             except:
-                return {"text":"Key wrong"}
+                return jsonify({"text":"Key wrong"})
         except:
-            return {"text":"Key Wrong"}
+            return jsonify({"text":"Key Wrong"})
     except:
         return "Error",500
 
